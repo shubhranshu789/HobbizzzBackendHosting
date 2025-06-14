96b5563d3d41960fdd001ce3c3dd46a2405c60b6
Nilesh Agrahari <nagrahari539@gmail.com>
2025-06-14 14:31:27 +0530

    g

--- model/director.js
+++ model/director.js
@@ -1,19 +1,10 @@
 const mongoose = require('mongoose');
 
 
-<<<<<<< HEAD:model/district.js
-const districtSchema = new mongoose.Schema({
-    name : { type:String, require:true} ,
-    total_members:{ type: Number, default: 0 },
-    Max_members:{
-        type: Number,
-        default: 1
-=======
 const directorSchema = new mongoose.Schema({
     name : {
         type:String,
         require:true
->>>>>>> 0feb76913e99939eed124d7daa62f8b2d01d351d:model/director.js
     },
     chapters: {
         type:Number,
@@ -36,8 +27,4 @@ const directorSchema = new mongoose.Schema({
 
 
 
-<<<<<<< HEAD:model/district.js
-module.exports=mongoose.model("DISTRICT" ,districtSchema );
-=======
 mongoose.model("DIRECTOR" ,directorSchema )
->>>>>>> 0feb76913e99939eed124d7daa62f8b2d01d351d:model/director.js
