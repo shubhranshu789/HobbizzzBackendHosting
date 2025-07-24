6f6307f4eddec6514e328ea6d14e62edf9913985
Nilesh Agrahari <nagrahari539@gmail.com>
2025-07-24 21:36:42 +0530

    commit

--- AllModels/techClubModel/techdirector.js
+++ AllModels/techClubModel/techdirector.js
@@ -18,10 +18,6 @@ const techdirectorSchema = new mongoose.Schema({
         type: String,
         require:true
     },
-    clubName: {
-        type: String,
-        required: true
-    },
     //add IP
     ip:{
         type: String,
--- routes/AllClubRoutes/TechClub/auth.js
+++ routes/AllClubRoutes/TechClub/auth.js
@@ -118,7 +118,7 @@ router.post("/TECHDIRECTOR-signup", async (req, res) => {
       ip,
       state,
       district,
-      clubName: clubName.toUpperCase()
+      club: clubName
     });
 
     const savedDirector = await director.save();
@@ -153,9 +153,9 @@ router.post("/TECHDIRECTOR-signin" , (req , res) => {
             if(match){
                 // return res.status(200).json({message :"Signed In Successufully" })
                 const token = jwt.sign({_id:savedUser.id} , Jwt_secret)
-                const {_id ,name , email , state , district , clubName } = savedUser
-                res.json({token , user:{_id ,name , email,  state , district , clubName  }})
-                console.log({token , user:{_id ,name , email ,  state , district , clubName}})
+                const {_id ,name , email , state , district , club } = savedUser
+                res.json({token , user:{_id ,name , email,  state , district , club }})
+                console.log({token , user:{_id ,name , email ,  state , district , club}})
             }else{
                 return res.status(422).json({error :"Invalid password" })
             }
@@ -202,7 +202,7 @@ router.post("/TECHEDITOR-signup", async (req, res) => {
       ip,
       state,
       district,
-      clubName: clubName.toUpperCase()
+      club: clubName
     });
 
     const savedDirector = await director.save();
@@ -238,9 +238,9 @@ router.post("/TECHEDITOR-signin" , (req , res) => {
             if(match){
                 // return res.status(200).json({message :"Signed In Successufully" })
                 const token = jwt.sign({_id:savedUser.id} , Jwt_secret)
-                const {_id ,name , email , state , district , clubName } = savedUser
-                res.json({token , user:{_id ,name , email,  state , district , clubName  }})
-                console.log({token , user:{_id ,name , email ,  state , district , clubName}})
+                const {_id ,name , email , state , district , club } = savedUser
+                res.json({token , user:{_id ,name , email,  state , district , club  }})
+                console.log({token , user:{_id ,name , email ,  state , district , club}})
             }else{
                 return res.status(422).json({error :"Invalid password" })
             }
@@ -255,88 +255,88 @@ router.post("/TECHEDITOR-signin" , (req , res) => {
 
 
 
-router.post("/TECHJUDGE-signup", async (req, res) => {
-  const { name, password, email, state, district, clubName } = req.body;
+// router.post("/TECHJUDGE-signup", async (req, res) => {
+//   const { name, password, email, state, district, clubName } = req.body;
 
-  const ip =
-    req.headers["cf-connecting-ip"] ||
-    req.headers["x-real-ip"] ||
-    req.headers["x-forwarded-for"] ||
-    req.socket.remoteAddress ||
-    "";
+//   const ip =
+//     req.headers["cf-connecting-ip"] ||
+//     req.headers["x-real-ip"] ||
+//     req.headers["x-forwarded-for"] ||
+//     req.socket.remoteAddress ||
+//     "";
 
-  if (!name || !password || !email || !state || !district || !clubName) {
-    return res.status(422).json({ error: "Please add all the fields" });
-  }
+//   if (!name || !password || !email || !state || !district || !clubName) {
+//     return res.status(422).json({ error: "Please add all the fields" });
+//   }
 
-  try {
-    const savedUser = await TECHJUDGE.findOne({
-      $or: [{ email: email }, { clubName: clubName }]
-    });
+//   try {
+//     const savedUser = await TECHJUDGE.findOne({
+//       $or: [{ email: email }, { clubName: clubName }]
+//     });
 
-    if (savedUser) {
-      return res
-        .status(422)
-        .json({ error: "User already exists with that email or club name" });
-    }
+//     if (savedUser) {
+//       return res
+//         .status(422)
+//         .json({ error: "User already exists with that email or club name" });
+//     }
 
-    const hashedPassword = await bcryptjs.hash(password, 12);
+//     const hashedPassword = await bcryptjs.hash(password, 12);
 
-    const director = new TECHJUDGE({
-      name,
-      email,
-      password: hashedPassword,
-      ip,
-      state,
-      district,
-      clubName: clubName.toUpperCase()
-    });
+//     const director = new TECHJUDGE({
+//       name,
+//       email,
+//       password: hashedPassword,
+//       ip,
+//       state,
+//       district,
+//       clubName: clubName.toUpperCase()
+//     });
 
-    const savedDirector = await director.save();
+//     const savedDirector = await director.save();
 
-    if (clubName.toUpperCase() === "TECH") {
-      const techClubId = "684a8c32d27f1ad8681187d0";
-      await TECHCLUB.findByIdAndUpdate(techClubId, {
-        $push: { director: savedDirector._id }
-      });
-    }
+//     if (clubName.toUpperCase() === "TECH") {
+//       const techClubId = "684a8c32d27f1ad8681187d0";
+//       await TECHCLUB.findByIdAndUpdate(techClubId, {
+//         $push: { director: savedDirector._id }
+//       });
+//     }
 
-    res.json({ message: "Judge registered successfully" });
-  } catch (err) {
-    console.error("Signup error:", err);
-    res.status(500).json({ error: "Server error" });
-  }
-});
-
-
+//     res.json({ message: "Judge registered successfully" });
+//   } catch (err) {
+//     console.error("Signup error:", err);
+//     res.status(500).json({ error: "Server error" });
+//   }
+// });
 
 
-router.post("/TECHJUDGE-signin" , (req , res) => {
-    const {email , password} = req.body;
 
-    if(!email || !password){
-        return res.status(422).json({error: "please add all the fields"})
-    }
 
-    TECHJUDGE.findOne({email:email}).then((savedUser) => {
-        if(!savedUser){
-            return res.status(422).json({error:"Invalid Email"})
-        }
-        bcryptjs.compare(password , savedUser.password).then((match) => {
-            if(match){
-                // return res.status(200).json({message :"Signed In Successufully" })
-                const token = jwt.sign({_id:savedUser.id} , Jwt_secret)
-                const {_id ,name , email , state , district , clubName } = savedUser
-                res.json({token , user:{_id ,name , email,  state , district , clubName  }})
-                console.log({token , user:{_id ,name , email ,  state , district , clubName}})
-            }else{
-                return res.status(422).json({error :"Invalid password" })
-            }
-        })
-        .catch(err => console.log(err))
-        // console.log(savedUser)
-    })
-})
+// router.post("/TECHJUDGE-signin" , (req , res) => {
+//     const {email , password} = req.body;
+
+//     if(!email || !password){
+//         return res.status(422).json({error: "please add all the fields"})
+//     }
+
+//     TECHJUDGE.findOne({email:email}).then((savedUser) => {
+//         if(!savedUser){
+//             return res.status(422).json({error:"Invalid Email"})
+//         }
+//         bcryptjs.compare(password , savedUser.password).then((match) => {
+//             if(match){
+//                 // return res.status(200).json({message :"Signed In Successufully" })
+//                 const token = jwt.sign({_id:savedUser.id} , Jwt_secret)
+//                 const {_id ,name , email , state , district , clubName } = savedUser
+//                 res.json({token , user:{_id ,name , email,  state , district , clubName  }})
+//                 console.log({token , user:{_id ,name , email ,  state , district , clubName}})
+//             }else{
+//                 return res.status(422).json({error :"Invalid password" })
+//             }
+//         })
+//         .catch(err => console.log(err))
+//         // console.log(savedUser)
+//     })
+// })
 
 
 
@@ -375,7 +375,7 @@ router.post("/TECHPRINCIPLE-signup", async (req, res) => {
       ip,
       state,
       district,
-      clubName: clubName.toUpperCase()
+      club: clubName
     });
 
     const savedDirector = await director.save();
@@ -412,9 +412,9 @@ router.post("/TECHPRINCIPLE-signin" , (req , res) => {
             if(match){
                 // return res.status(200).json({message :"Signed In Successufully" })
                 const token = jwt.sign({_id:savedUser.id} , Jwt_secret)
-                const {_id ,name , email , state , district , clubName } = savedUser
-                res.json({token , user:{_id ,name , email,  state , district , clubName  }})
-                console.log({token , user:{_id ,name , email ,  state , district , clubName}})
+                const {_id ,name , email , state , district , club } = savedUser
+                res.json({token , user:{_id ,name , email,  state , district , club  }})
+                console.log({token , user:{_id ,name , email ,  state , district , club}})
             }else{
                 return res.status(422).json({error :"Invalid password" })
             }
